a91ee447464aa11e531c215f5b46c38253331022
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-08-22 18:31:39 -0300

    _

---
+++ JavaScript/js/sintaxe.js
@@ -0,0 +1,16 @@
+//Number
+console.log(5+5);
+//String
+console.log("5+5");
+
+// Criação de variáveis: let, const, var
+
+var a = 1;
+var b = 5;
+var c = a+b;
+console.log(c);
+
+//Case sensitive, Letras minúculas e maiúsculas são diferentes.
+
+var VICTOR = 5;
+var victor = 6;
---
+++ JavaScript/js/variaveis.js
@@ -0,0 +1,32 @@
+/*Variávies*/
+
+//VAR
+
+//LET
+
+//CONST
+
+var pote = "Bombom";
+
+//Declaração de variáveis
+var a,b,c;
+
+//Atribuição de valores
+a = 2;
+b = 3;
+c = a+b;
+
+//var pode reatribuir valores
+var a = 8;
+var nome, sobrenome;
+nome = "Victor";
+sobrenome = "Cesar"
+
+//Concatenação
+var nomeCompleto = nome + sobrenome;
+
+//let torna a variável única, não pode receber outro valor,valerá em todo o código obedecendo o escopo.
+let y = 9;
+
+//invariável, não adimite alteração em seu valor.
+const t = 7;
