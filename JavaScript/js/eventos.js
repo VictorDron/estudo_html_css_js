60d5061026dfeeaa2dc718f8566110f24cfc04b3
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-08-25 16:49:02 -0300

    ok

---
+++ JavaScript/js/eventos.js
@@ -0,0 +1,23 @@
+/*
+onclick
+ondblclick
+onmouseover
+onmouseout
+onmousemove
+onmousedown
+onmouseup
+onfocus
+onchange
+onblur
+onkeydown
+onkeypress
+onkeyup
+onload
+onresize
+
+*/
+
+function clickEvento(){
+    alert('ACIONOU UM EVENTO DE CLICK');
+
+}
