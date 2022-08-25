bccc89a2db88ab92ff98a4b7642e6ccf5389eefe
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-08-25 19:44:11 -0300

    ok

--- JavaScript/js/metodosarrays.js
+++ JavaScript/js/metodosarrays.js
@@ -2,6 +2,7 @@ function print(x,y) {
     return document.getElementById(x).innerHTML = y;
 }
 const pessoa = ["pão","queijo","alameda"];
+const lista = ["suco","laranja","maracuja"];
 
 //document.getElementById(x).innerHTML = y;
 
@@ -17,4 +18,40 @@ pessoa.shift();
 pessoa.unshift("bonitão");
 //Deleta um elemneto em específico
 delete pessoa [0];
-print("texto",pessoa);
+//Adiciona com o poder de subtituição 
+pessoa.splice();
+//Concatenação de arrays
+const uniao = pessoa.concat(lista);
+
+const jogadores = ["pele","rodrigo","alef","messi","carlos"];
+
+//Fatia em posições
+const craques = jogadores.slice(2);
+//Ordem alfabética
+jogadores.sort();
+//Reverte a ordem alfabetica
+jogadores.reverse();
+ 
+const numeros = [100,20,600,55];
+//Ordem crescente
+numeros.sort(function(a,b) {return a-b});
+//Ordem decrescente
+numeros.sort(function(a,b) {return b-a});
+//Maior número
+function maiorNumero (array){
+    return Math.max.apply(null,array);
+}
+//Menos numero
+function menorNumero (array){
+
+    return Math.min.apply(null,array);
+}
+//Filtro
+function filtragem (value,index,array){
+    return value > 20;
+}
+numeros.filter(filtragem);
+
+
+
+print("texto",craques);
