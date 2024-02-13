9d727f803dbe9c73169c0bf3989c5cf3ce2995e6
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2024-02-13 16:48:29 -0300

    Json

--- JavaScript/js/json.js
+++ JavaScript/js/json.js
@@ -1,3 +1,7 @@
+//JSON = JavaScript Object Notation
+
+//Transmissão de dados
+
 //Converte texto no padrão JSON em objetos.
 //JSON.parse();
 //Converte objetos em texto padrão JSON.
@@ -7,13 +11,14 @@ const carro = {
 
     marca: "Fiat",
     modelo:"Uno",
-    ano:2001
+    ano:2001,
+    motor: ["v1", "v3","V4"]
 }
 
-function print(y){
 
-    document.getElementById('area').innerHTML = y;
-} 
-
+//Converte o Objeto em texto
 let texto = JSON.stringify(carro);
-print(texto);
+//Converte o texto em JSON para tratar o elementos
+let obj = JSON.parse(texto);
+
+console.log(obj.marca);
